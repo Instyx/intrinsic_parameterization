03e94b48715c9494d894c0be78a4c0ca25e84639
Ugo Finnendahl <finnendahl@tu-berlin.de>
2024-01-24 01:15:07 +0100

    rewrote runner.py

--- runner.py
+++ runner.py
@@ -1,13 +1,11 @@
 import os, glob, sys, math
 
 methods = ["dirichlet", "asap", "symdirichlet", "arap"]
-delaunay = sys.argv[1] if len(sys.argv) > 1 else "nodelaunay"
-prio = sys.argv[2] if len(sys.argv) > 2 else "noprio"
-start = int(sys.argv[3]) if len(sys.argv) > 3 else 0
-step = int(sys.argv[4]) if len(sys.argv) > 4 else 1
-end = int(sys.argv[5]) if len(sys.argv) > 5 else None
+start = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+step = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+end = int(sys.argv[3]) if len(sys.argv) > 3 else None
 folder = "thingi_data"
-output = "output_"+delaunay+"_"+prio
+output = "output"
 
 num = math.floor((len(glob.glob(f"{folder}/*.off")[:end])-1-start)/step)
 
@@ -18,7 +16,25 @@ for k,i in enumerate(sorted(glob.glob(f"{folder}/*.off"), key = os.path.getsize)
     print("------------------------------------------------------------------------------------")
     print("------------------------------------------------------------------------------------")
     for m in methods:
-        cmd = f"build/cli {m} {i} {output} {delaunay} {prio}"
+        cmd = f"build/cli {m} {i} {output} nodelaunay noprio"
+        print(cmd)
+        try:
+            sts = os.system(cmd)
+            if(sts == 2):
+                sys.exit(0)
+        except Exception as e:
+            print("Failed method", m, "for mesh", i)
+        print()
+        cmd = f"build/cli {m} {i} {output}_delaunay delaunay noprio"
+        print(cmd)
+        try:
+            sts = os.system(cmd)
+            if(sts == 2):
+                sys.exit(0)
+        except Exception as e:
+            print("Failed method", m, "for mesh", i)
+        print()
+        cmd = f"build/cli {m} {i} {output}_prio nodelaunay prio"
         print(cmd)
         try:
             sts = os.system(cmd)
