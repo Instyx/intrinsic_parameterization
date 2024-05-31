79e9dba2fc567b9834c92f292eeb69b399226ada
Koray Akalin <koray.akalin2001@gmail.com>
2024-05-31 23:03:14 +0200

    for random flip

--- include/optimize.hpp
+++ include/optimize.hpp
@@ -16,8 +16,18 @@ struct Results{
   std::vector<double> flip_durations;
   std::vector<double> opt_durations;
   std::vector<int> opt_iterations;
+  std::vector<double> random_flip_energies;
+  double pq_flip_energy;
+  double q_flip_energy;
+  std::vector<int> random_flip_iterations;
+  int pq_flip_iteration;
+  int q_flip_iteration;
+
 };
 
 Results optimize_single(Eigen::MatrixXd &V, Eigen::MatrixXi &F, EnergyType method, std::string dir, std::string mesh_name);
 
 Results optimize_single(Eigen::MatrixXd &V, Eigen::MatrixXi &F, EnergyType method, std::string dir, std::string mesh_name, const bool init_with_intrinsic, const bool priority_queue_flips);
+
+
+Results optimize_single_random(Eigen::MatrixXd &V, Eigen::MatrixXi &F, EnergyType method, std::string dir, std::string mesh_name, const bool init_with_intrinsic, unsigned random_runs);
--- include/optimize_new.hpp
+++ include/optimize_new.hpp
@@ -7,3 +7,5 @@
 Results optimize_single_new(Eigen::MatrixXd &V, Eigen::MatrixXi &F, EnergyType method, std::string dir, std::string mesh_name);
 
 Results optimize_single_new(Eigen::MatrixXd &V, Eigen::MatrixXi &F, EnergyType method, std::string dir, std::string mesh_name, const bool init_with_intrinsic, const bool priority_queue_flips);
+
+Results optimize_single_new(Eigen::MatrixXd &V, Eigen::MatrixXi &F, EnergyType method, std::string dir, std::string mesh_name, const bool init_with_intrinsic, const bool priority_queue_flips, const bool rando);
